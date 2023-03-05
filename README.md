0c65a3886f99b9771051ff39711dd22721cb1b71
João Gabriel <joaogabriel9633@gmail.com>
2023-03-05 20:34:31 -0300

    Add Files

---
+++ README.md
@@ -0,0 +1,4 @@
+Hello, John here!
+This simple project was made together with one bit code. Enjoy and see how I managed to do similar.
+
+Technologies used: HTML and CSS
---
+++ js/script.js
@@ -0,0 +1,5 @@
+const clear = document.querySelector('.clear');
+
+const datElement = document.getElementById('date')
+const list = document.getElementById('list')
+const input = document.getElementById('input')
